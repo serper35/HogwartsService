5dc2b8c3fd300179449423fd7f1e434b7c86227e
serper35 <serper.wolf@gmail.com>
2024-02-04 00:04:11 +0300

    added avatar service

--- school/src/main/java/ru/hogwarts/school/model/Avatar.java
+++ school/src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -2,6 +2,9 @@ package ru.hogwarts.school.model;
 
 import jakarta.persistence.*;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Entity
 public class Avatar {
     @Id
@@ -63,4 +66,30 @@ public class Avatar {
         this.student = student;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Avatar avatar = (Avatar) o;
+        return fileSize == avatar.fileSize && Objects.equals(id, avatar.id) && Objects.equals(filePath, avatar.filePath) && Objects.equals(mediaType, avatar.mediaType) && Arrays.equals(data, avatar.data) && Objects.equals(student, avatar.student);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, filePath, fileSize, mediaType, student);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" +
+                "id=" + id +
+                ", filePath='" + filePath + '\'' +
+                ", fileSize=" + fileSize +
+                ", mediaType='" + mediaType + '\'' +
+                ", data=" + Arrays.toString(data) +
+                ", student=" + student +
+                '}';
+    }
 }
